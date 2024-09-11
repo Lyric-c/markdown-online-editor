bbe10bfc2b782fdc57402a989fdaa3862f25bfea
cczhao <cchuanzhao@gmail.com>
2024-09-11 14:35:32 +0800

    123

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import './global.js'
 import App from './App.vue'
 import router from './router'
-import './registerServiceWorker'
+// import './registerServiceWorker'
 
 new Vue({
   router,
