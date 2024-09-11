cad39babb246a8a45b3b126ec8359ddfe8788a0b
cczhao <cchuanzhao@gmail.com>
2024-09-11 11:44:41 +0800

    1

---
+++ src/pages/About.vue
@@ -0,0 +1,35 @@
+<!-- @format -->
+
+<template>
+  <PreviewVditor :pdata="pdata" @callback="handleCallBack" />
+</template>
+
+<script>
+import PreviewVditor from '@components/PreviewVditor'
+import AboutAryaMd from '@config/aboutArya'
+
+export default {
+  name: 'about-arya',
+
+  data() {
+    return {
+      isLoading: false,
+      pdata: AboutAryaMd
+    }
+  },
+
+  created() {},
+
+  components: {
+    PreviewVditor
+  },
+
+  mounted() {},
+
+  methods: {
+    handleCallBack() {
+      this.isLoading = false
+    }
+  }
+}
+</script>
